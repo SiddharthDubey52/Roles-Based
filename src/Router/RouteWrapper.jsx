04a91a3db1c07eaf894f0296a6a3721f3d9402b7
siddharthdubey52 <dubeysiddharth368@gmail.com>
2025-11-02 13:15:58 +0530

    Fix issue test-01

--- src/Router/RouteWrapper.jsx
+++ src/Router/RouteWrapper.jsx
@@ -3,14 +3,14 @@ import { Route, Routes } from 'react-router-dom'
 
 import Dash from '../component/Dasboard/Dash'
 import Project from '../component/Project/Project'
-import Task from '../component/Task/Task'
-import Login from '../component/Login/Login'
+
+import Register from '../component/Login/Register'
 
 const RouteWrapper = () => {
   return (
    <>
    <Routes>
-    <Route path='/' element={<Login/>}/>
+    <Route path='/' element={<Register/>}/>
     <Route path='/dashboard' element={<Dash />} />
     <Route path='/projects' element={<Dash><Project /></Dash>} />
     <Route path='/tasks' element={<Dash><Task /></Dash>} />
