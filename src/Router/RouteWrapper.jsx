92ecc00595d5b8fa164eb5acb40213d86fa2e1ae
siddharthdubey52 <dubeysiddharth368@gmail.com>
2025-11-02 13:05:43 +0530

    fix for deployment

--- src/Router/RouteWrapper.jsx
+++ src/Router/RouteWrapper.jsx
@@ -1,15 +1,16 @@
 import React from 'react'
 import { Route, Routes } from 'react-router-dom'
-import Login from '../component/Login/Login'
+
 import Dash from '../component/Dasboard/Dash'
 import Project from '../component/Project/Project'
 import Task from '../component/Task/Task'
+import Login from '../component/login/login'
 
 const RouteWrapper = () => {
   return (
    <>
    <Routes>
-    <Route path="/" element={<Login />} />
+    <Route path='/' element={<Login/>}/>
     <Route path='/dashboard' element={<Dash />} />
     <Route path='/projects' element={<Dash><Project /></Dash>} />
     <Route path='/tasks' element={<Dash><Task /></Dash>} />
